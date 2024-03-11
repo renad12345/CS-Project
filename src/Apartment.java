63d2c5787d8159a2d96f6ff84f622d3fac9a92ad
96650 <96650@LAPTOP-IG6PM96P>
2024-03-12 02:40:02 +0300

    Merge branch 'main' of https://github.com/renad12345/CS-Project into

--- src/Apartment.java
+++ src/Apartment.java
@@ -1,14 +1,49 @@
 
 public class Apartment extends Property{
 private int  NumOfRooms ;
+protected int PricOf1meter = 800;
 
-public Apartment() {
-	super();
-	// TODO Auto-generated constructor stub
+
+
+public Apartment(String propertyNum, String city, String region, String district, String street, int area,
+		boolean rented, int numOfRooms) {
+	super(propertyNum, city, region, district, street, area, rented);
+	NumOfRooms = numOfRooms;
 }
 
 
+public double CalculatePrice() {
+if (NumOfRooms<2)
+return (PricOf1meter * Area)+(NumOfRooms*1000)
+else
+	return (PricOf1meter * Area)	
+}
 
+
+@Override
+public String toString() {
+	return super.toString()+"Apartment [NumOfRooms=" + NumOfRooms + ", PricOf1meter=" + PricOf1meter + "]";
+}
+
+
+public int getNumOfRooms() {
+	return NumOfRooms;
+}
+
+
+public int getPricOf1meter() {
+	return PricOf1meter;
+}
+
+
+public void setNumOfRooms(int numOfRooms) {
+	NumOfRooms = numOfRooms;
+}
+
+
+public void setPricOf1meter(int pricOf1meter) {
+	PricOf1meter = pricOf1meter;
+}
 
 
 
