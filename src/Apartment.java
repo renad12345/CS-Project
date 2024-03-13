7f9939a20fce6b2d43679f94c08af83d4419a432
96650 <96650@LAPTOP-IG6PM96P>
2024-03-13 03:22:08 +0300

    ---

--- src/Apartment.java
+++ src/Apartment.java
@@ -1,4 +1,63 @@
 
-public class Apartment {
+public class Apartment extends Property{
+private int  NumOfRooms ;
+//#
+protected int PricOf1meter = 800;
+
+
+
+public Apartment(String tanentName, int propertyID, String city, String region, String district, String street,
+		double area, boolean rented, int numOfRooms, int pricOf1meter) {
+	super(tanentName, propertyID, city, region, district, street, area, rented);
+	NumOfRooms = numOfRooms;
+	PricOf1meter = pricOf1meter;
+}
+
+
+
+
+
+//copyConstructor
+public Apartment(Apartment Ap) {
+	super(Ap.tanentName,Ap.propertyID , Ap.city, Ap.region, Ap.district, Ap.street, Ap.area , Ap.rented);
+	NumOfRooms = Ap.NumOfRooms;
+	PricOf1meter = Ap.PricOf1meter;		
+	
+}
+
+
+
+public double calculateDailyRent() {
+if (NumOfRooms<2)
+return (PricOf1meter * area)+(NumOfRooms*1000);
+else
+	return (PricOf1meter * area)	;
+}
+
+
+@Override
+public String toString() { //#
+	return super.toString()+"Apartment [NumOfRooms=" + NumOfRooms + ", PricOf1meter=" + PricOf1meter + "]";
+}
+
+
+public int getNumOfRooms() {
+	return NumOfRooms;
+}
+
+
+public int getPricOf1meter() {
+	return PricOf1meter;
+}
+
+
+public void setNumOfRooms(int numOfRooms) {
+	NumOfRooms = numOfRooms;
+}
+
+
+public void setPricOf1meter(int pricOf1meter) {
+	PricOf1meter = pricOf1meter;
+}
 
 }
--- src/User.java
+++ src/User.java
@@ -1,4 +1,165 @@
+import java.util.Arrays;
 
 public class User {
+	private String ID ;
+	private String name;
+	private String phone;
+	private int numOfLease ; 
+	private int numOfProperty;
+	Lease leases [] ;
+	Property properties [] ;
+	
+	public User(String ID, String name, String phone, int leasesSize, int propertiesSize) {
+		this.ID = ID;
+		this.name = name;
+		this.phone = phone;
 
+		leases = new Lease [leasesSize];
+		properties = new Property [propertiesSize];		
+		
+		numOfLease = 0;
+		numOfProperty = 0;
+	}
+	
+	
+	public boolean addLease (Lease L) {
+		if (numOfLease<leases.length)
+		{
+			leases[numOfLease++]=new Lease(L);
+			 return true;	
+		}
+		return false;	
+		
+	}
+	//
+	public Lease SearchLease(int LeaseID) {
+		for(int i = 0 ;i < numOfLease ; i++)
+		if(leases[i].getLeaseID()== LeaseID )
+		return leases[i]; 
+	
+		
+		return null;
+			
+	}
+	
+	
+	
+	public boolean  removeLease (int LeaseID) {
+		
+		for(int i = 0 ;i < numOfLease ; i++)
+			if(leases[i].getLeaseID() == LeaseID) {
+				
+				for(int j = i ; j < numOfLease -1 ; j++) 
+					leases[j] = leases[j+1] ;
+				numOfLease-- ;
+				leases[numOfLease ]= null;	
+				
+				return true;
+			}
+		return false;	
+		
+	}
+	
+	
+	
+	
+	// 
+	public boolean addProperty(  Property p ) {
+	if (properties.length < numOfProperty )
+	{
+	if (p instanceof Shop  )
+		properties[numOfProperty++]=new Shop ((Shop) p);
+		else 
+	if (p instanceof Apartment  )
+    properties[numOfProperty++]=new Apartment ((Apartment) p)	;	
+		
+		
+	
+	return true ;	
+	}
+	return false ;	
+		
+	}
+	
+	
+	
+	
+public boolean  removeProperty ( int propertyId) {
+		
+		for(int i = 0 ;i < numOfProperty ; i++)
+			if(properties[i].getPropertyID()==propertyId) {
+				
+				for(int j = i ; j < numOfProperty -1 ; j++) 
+					properties[j] = properties[j+1] ;
+				numOfProperty-- ;
+				properties[numOfProperty ]= null;	
+				
+				return true;
+			}
+		return false;	
+		
+	}
+	
+	public Property findProperty( int propertyid) {
+		for(int i = 0 ; i < numOfProperty ; i++)
+			if(properties[i].propertyID ==(propertyid) )
+			
+			return properties[i]; 
+		
+			return null ; 
+			}
+		
+	
+	public boolean availableProperties() {
+	
+		for(int i = 0 ; i < numOfProperty ; i++)
+		if (rented())	
+		return false;
+		
+		return true; 
+		
+	}
+
+
+	public String getID() {
+		return ID;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public int getNumOfLease() {
+		return numOfLease;
+	}
+
+
+	public int getNumOfproperty() {
+		return numOfProperty;
+	}
+
+
+	@Override
+	public String toString() { //#
+		return "User [id=" + ID + ", name=" + name + ", phone=" + phone + ", numOfLease=" + numOfLease
+				+ ", numOfproperty=" + numOfProperty + ", leases=" + Arrays.toString(leases) + ", properties="
+				+ Arrays.toString(properties) + "]";
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
 }
